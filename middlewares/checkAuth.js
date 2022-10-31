e50b2c434c20e5153217ad9b5f394cd32bfa7219
unknown <rostanets@gmail.com>
2022-11-01 00:51:52 +0200

    upd

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -8,7 +8,7 @@ module.exports = function (req, res, next) {
     if (req.originalUrl === '/admin/login') {
       next();
     } else {
-      res.redirect('admin/login');
+      res.redirect('/admin/login');
     }
   }
 }
