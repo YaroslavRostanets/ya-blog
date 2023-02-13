610b87f15b58a2ef9b4c251284c3bc9cdb64566c
unknown <rostanets@gmail.com>
2023-02-13 12:57:51 +0200

    upd

--- models/user.js
+++ models/user.js
@@ -36,7 +36,6 @@ const User = database.define('User', {
 
 User.getUser = async function (login, password) {
   const passwordHash = crypto.createHash('md5').update(password).digest("hex");
-  // const passwordHash = password;
   return await User.findOne({
     attributes: ['id', 'firstName', 'lastName'],
     limit: 1,
